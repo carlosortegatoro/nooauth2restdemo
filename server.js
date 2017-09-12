921bbcc889909d28deaf4f1eb13265022c5089bd
Carlos Ortega <carlos.ortega@carlosorte-ltm.internal.salesforce.com>
2017-09-12 12:52:37 +0200

    change to port 143

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 80,
+  port = process.env.PORT || 143,
   //mongoose = require('mongoose'),
   //Task = require('./api/models/todoListModel'), //created model loading here
   bodyParser = require('body-parser');
