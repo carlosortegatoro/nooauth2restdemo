57a755b9b74320177b6dc3a99bab3a2cfd8afd3f
carlosortegatoro <carlosortegatoro@gmail.com>
2017-09-17 21:59:39 +0200

    Adding post

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ app.use(bodyParser.json());
 
 app.listen(port);
 app.get('/get',getResponse);
+app.post('/post',postResponse);
 
 function getResponse(req, res, next) {
 	res.json({ message : 'everything is ok'});
 }
 
+function postResponse(req, res, next) {
+	res.json({ message : 'everything is ok'});
+}
 
 console.log('todo list RESTful API server started on: ' + port);
